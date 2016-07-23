1da28f9b76fdbd3382eb47c85d1febb4addf13fa
Simon Dahlberg <Simon Dahlberg>
2016-07-23 20:24:44 +0200

    Tog bort lite skit. La till Naget-klappar

---
+++ BetterTomorrow/Network/HttpRequestService.cs
@@ -0,0 +1,18 @@
+using System.Net.
+
+namespace BetterTomorrow.Network
+{
+	public class HttpRequestService
+	{
+
+		public HttpRequestService()
+		{
+			HttpClient awd;
+		}
+
+		public string Get(string url, string request)
+		{
+			return "";
+		}
+	}
+}
